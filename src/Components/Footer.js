e8e6d12b6b9d458129150f1b63bebfd8632ee46a
Md Tobibur Rohman <71486299+wptobibur2021@users.noreply.github.com>
2022-03-13 22:37:27 +0600

    Ui

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 12, sm: 12, md: 12 }}>
                 <Grid item xs={12} sm={12} md={12} sx={{ py: 3, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                     <Typography sx={{ textAlign: 'center' }} variant="p">
-                        &copy; Copyright {document.write(new Date().getFullYear())}. Design & Development by <a href="https://techbd71.com/" target='_blank'>TechBD71</a>
+                        &copy; Copyright 2022. Design & Development by <a href="https://techbd71.com/" target='_blank'>TechBD71</a>
                     </Typography>
                 </Grid>
             </Grid>
